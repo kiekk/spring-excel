bbd8404382c67148789970dae70d92103d594b61
soonho <dbstnsgh2@naver.com>
2022-05-19 23:12:46 +0900

    add entityToMap to Board

--- src/main/java/com/poi/excel/entity/Board.java
+++ src/main/java/com/poi/excel/entity/Board.java
@@ -3,15 +3,18 @@ package com.poi.excel.entity;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Board {
 
-    private String id;
+    private int id;
     private String title;
     private String content;
     private String writer;
@@ -19,5 +22,18 @@ public class Board {
     private int likeIt;
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
+
+    public Map<String, Object> entityToMap() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("title", title);
+        result.put("content", content);
+        result.put("writer", writer);
+        result.put("viewCount", viewCount);
+        result.put("likeIt", likeIt);
+        result.put("createDate", createDate);
+        result.put("updateDate", updateDate);
+        return result;
+    }
 
 }
