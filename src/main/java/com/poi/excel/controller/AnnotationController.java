240d1140409028bb7fdd771202222a040a2f6daf
soonho <dbstnsgh2@naver.com>
2022-05-20 22:42:38 +0900

    annotation: add AnnotationController

---
+++ src/main/java/com/poi/excel/controller/AnnotationController.java
@@ -0,0 +1,60 @@
+package com.poi.excel.controller;
+
+import com.poi.excel.entity.Board;
+import com.poi.excel.util.annotation.ExcelDownload;
+import com.poi.excel.util.annotation.ExcelWriter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/annotation")
+public class AnnotationController {
+
+    private final static List<Board> boardList = new ArrayList<>();
+
+    static {
+        boardList.add(
+                Board.builder().id(1).title("게시글1").content("게시글내용1").writer("작성자1")
+                        .viewCount(0).likeIt(1).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(2).title("게시글2").content("게시글내용2").writer("작성자2")
+                        .viewCount(1).likeIt(2).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(3).title("게시글3").content("게시글내용3").writer("작성자3")
+                        .viewCount(2).likeIt(3).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(4).title("게시글4").content("게시글내용4").writer("작성자4")
+                        .viewCount(3).likeIt(4).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(5).title("게시글5").content("게시글내용5").writer("작성자5")
+                        .viewCount(4).likeIt(5).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(6).title("게시글6").content("게시글내용6").writer("작성자6")
+                        .viewCount(5).likeIt(6).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(7).title("게시글7").content("게시글내용7").writer("작성자7")
+                        .viewCount(6).likeIt(7).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(8).title("게시글8").content("게시글내용8").writer("작성자8")
+                        .viewCount(7).likeIt(8).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(9).title("게시글9").content("게시글내용9").writer("작성자9")
+                        .viewCount(8).likeIt(9).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        boardList.add(
+                Board.builder().id(10).title("게시글10").content("게시글내용10").writer("작성자10")
+                        .viewCount(9).likeIt(10).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+    }
+
+    @RequestMapping("excel-download")
+    public ModelAndView poiExcelDownload1() throws IOException {
+        Map<String, Object> excelData = ExcelWriter.createExcelData(boardList, Board.class);
+        return new ModelAndView(new ExcelDownload(), excelData);
+    }
+}
