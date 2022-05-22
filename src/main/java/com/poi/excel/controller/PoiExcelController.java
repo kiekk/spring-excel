f4cb8c9487a82b366db54c1ad583856fd8f3974e
soonho <dbstnsgh2@naver.com>
2022-05-22 22:29:12 +0900

    poi-excel-upload: add function excel-upload

--- src/main/java/com/poi/excel/controller/PoiExcelController.java
+++ src/main/java/com/poi/excel/controller/PoiExcelController.java
@@ -2,15 +2,21 @@ package com.poi.excel.controller;
 
 import com.poi.excel.entity.Board;
 import com.poi.excel.util.poi.ExcelUtil;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -35,9 +41,88 @@ public class PoiExcelController {
         headerList1.add(new Board().getHeaderToMap());
     }
 
-    @RequestMapping("excel-upload")
+    @GetMapping("excel-upload")
     public String poiExcelUpload() {
         return "poi-excel-upload";
+    }
+
+    @PostMapping("excel-upload")
+    public void poiExcelUpload(MultipartFile excelFile) {
+        List<Board> boardList = new ArrayList<>();
+        try {
+            OPCPackage opcPackage = OPCPackage.open(excelFile.getInputStream());
+            XSSFWorkbook workbook = new XSSFWorkbook(opcPackage);
+
+            // 첫 번째 시트 불러오기
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 1; i < sheet.getLastRowNum() + 1; i++) {
+                Board board = new Board();
+                XSSFRow row = sheet.getRow(i);
+                int rowNum = 0;
+
+                if (null == row) {
+                    continue;
+                }
+
+                // 행의 1번째 열
+                XSSFCell cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setId(Integer.parseInt(cell.getStringCellValue()));
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setTitle(cell.getStringCellValue());
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setTitle(cell.getStringCellValue());
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setContent(cell.getStringCellValue());
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setWriter(cell.getStringCellValue());
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setViewCount(Integer.parseInt(cell.getStringCellValue()));
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setLikeIt(Integer.parseInt(cell.getStringCellValue()));
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setCreateDate(cell.getDateCellValue().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+                }
+
+                cell = row.getCell(rowNum++);
+                if (null != cell) {
+                    board.setUpdateDate(cell.getDateCellValue().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime());
+                }
+
+                boardList.add(board);
+            }
+        } catch (InvalidFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("################################################");
+        System.out.println("################################################");
+        System.out.println("################################################");
+        System.out.println("################################################");
+        System.out.println("################################################");
+        boardList.forEach(System.out::println);
     }
 
     @RequestMapping("excel-download-1")
@@ -81,11 +166,11 @@ public class PoiExcelController {
         //Font
         Font fontHeader = workbook.createFont();
         fontHeader.setFontName("맑은 고딕");
-        fontHeader.setFontHeight((short)(9 * 20));
+        fontHeader.setFontHeight((short) (9 * 20));
         fontHeader.setBold(true);
         Font font9 = workbook.createFont();
         font9.setFontName("맑은 고딕");
-        font9.setFontHeight((short)(9 * 20));
+        font9.setFontHeight((short) (9 * 20));
 
         // 엑셀 헤더 셋팅
         XSSFCellStyle headerStyle = workbook.createCellStyle();
@@ -135,7 +220,7 @@ public class PoiExcelController {
             sheet.setColumnWidth(i, colWidths[i]);
         }
         //데이터 부분 생성
-        for(Board board : boardList2) {
+        for (Board board : boardList2) {
             cellCnt = 0;
             row = sheet.createRow(rowCnt++);
             // id
@@ -184,9 +269,9 @@ public class PoiExcelController {
         response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
         try {
             workbook.write(response.getOutputStream());
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
