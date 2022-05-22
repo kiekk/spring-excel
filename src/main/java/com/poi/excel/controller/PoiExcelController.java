843f6403122f94ed7a279523b14fb7efc95b4771
soonho <dbstnsgh2@naver.com>
2022-05-22 22:16:48 +0900

    poi-excel-upload: 초기 데이터 설정 로직 수정

--- src/main/java/com/poi/excel/controller/PoiExcelController.java
+++ src/main/java/com/poi/excel/controller/PoiExcelController.java
@@ -24,69 +24,15 @@ public class PoiExcelController {
     private final static List<Board> boardList2 = new ArrayList<>();
 
     static {
-        boardList1.add(
-                Board.builder().id(1).title("게시글1").content("게시글내용1").writer("작성자1")
-                        .viewCount(0).likeIt(1).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(2).title("게시글2").content("게시글내용2").writer("작성자2")
-                        .viewCount(1).likeIt(2).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(3).title("게시글3").content("게시글내용3").writer("작성자3")
-                        .viewCount(2).likeIt(3).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(4).title("게시글4").content("게시글내용4").writer("작성자4")
-                        .viewCount(3).likeIt(4).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(5).title("게시글5").content("게시글내용5").writer("작성자5")
-                        .viewCount(4).likeIt(5).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(6).title("게시글6").content("게시글내용6").writer("작성자6")
-                        .viewCount(5).likeIt(6).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(7).title("게시글7").content("게시글내용7").writer("작성자7")
-                        .viewCount(6).likeIt(7).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(8).title("게시글8").content("게시글내용8").writer("작성자8")
-                        .viewCount(7).likeIt(8).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(9).title("게시글9").content("게시글내용9").writer("작성자9")
-                        .viewCount(8).likeIt(9).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-        boardList1.add(
-                Board.builder().id(10).title("게시글10").content("게시글내용10").writer("작성자10")
-                        .viewCount(9).likeIt(10).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
-
+        for (int i = 1; i < 10; i++) {
+            boardList1.add(
+                    Board.builder().id(i).title("게시글_" + i).content("게시글내용_" + i).writer("작성자_" + i)
+                            .viewCount(i).likeIt(i).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build().entityToMap());
+            boardList2.add(
+                    Board.builder().id(i).title("게시글_" + i).content("게시글내용_" + i).writer("작성자_" + i)
+                            .viewCount(i).likeIt(i).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
+        }
         headerList1.add(new Board().getHeaderToMap());
-
-        boardList2.add(
-                Board.builder().id(1).title("게시글1").content("게시글내용1").writer("작성자1")
-                        .viewCount(0).likeIt(1).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(2).title("게시글2").content("게시글내용2").writer("작성자2")
-                        .viewCount(1).likeIt(2).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(3).title("게시글3").content("게시글내용3").writer("작성자3")
-                        .viewCount(2).likeIt(3).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(4).title("게시글4").content("게시글내용4").writer("작성자4")
-                        .viewCount(3).likeIt(4).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(5).title("게시글5").content("게시글내용5").writer("작성자5")
-                        .viewCount(4).likeIt(5).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(6).title("게시글6").content("게시글내용6").writer("작성자6")
-                        .viewCount(5).likeIt(6).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(7).title("게시글7").content("게시글내용7").writer("작성자7")
-                        .viewCount(6).likeIt(7).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(8).title("게시글8").content("게시글내용8").writer("작성자8")
-                        .viewCount(7).likeIt(8).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(9).title("게시글9").content("게시글내용9").writer("작성자9")
-                        .viewCount(8).likeIt(9).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
-        boardList2.add(
-                Board.builder().id(10).title("게시글10").content("게시글내용10").writer("작성자10")
-                        .viewCount(9).likeIt(10).createDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build());
     }
 
     @RequestMapping("excel-download-1")
