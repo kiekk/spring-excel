829d904588d06567f1aa7211748fd46f875c9cc8
soonho <dbstnsgh2@naver.com>
2022-05-23 21:20:39 +0900

    jxls-excel-upload: add function excel-upload

--- src/main/java/com/poi/excel/controller/JxlsController.java
+++ src/main/java/com/poi/excel/controller/JxlsController.java
@@ -2,16 +2,30 @@ package com.poi.excel.controller;
 
 import com.poi.excel.entity.Board;
 import com.poi.excel.entity.User;
+import com.poi.excel.util.LocalDateTimeConverter;
 import com.poi.excel.util.jxls.JxlsExcelDownload;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.jxls.reader.ReaderBuilder;
+import org.jxls.reader.XLSReader;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.xml.sax.SAXException;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/jxls")
@@ -52,4 +66,31 @@ public class JxlsController {
         return "jxls-excel-upload";
     }
 
+    @PostMapping("excel-upload")
+    @ResponseBody
+    public void jxlsExcelUpload(MultipartFile excelFile) {
+        ClassPathResource classPathResource = new ClassPathResource("static/excel/xml/board.xml");
+        List<Board> uploadBoardList = new ArrayList<>();
+
+        try (InputStream inputStream = new BufferedInputStream(classPathResource.getInputStream());
+             InputStream multipartInputStream = excelFile.getInputStream()) {
+            XLSReader xlsReader = ReaderBuilder.buildFromXML(inputStream);
+            xlsReader.getConvertUtilsBeanProvider().getConvertUtilsBean().register(new LocalDateTimeConverter(), java.time.LocalDateTime.class);
+
+            Map<String, Object> beans = new HashMap<>();
+            beans.put("boardList", uploadBoardList);
+
+            xlsReader.read(multipartInputStream, beans);
+
+            System.out.println("##########################");
+            System.out.println("##########################");
+            System.out.println("##########################");
+            System.out.println("##########################");
+            uploadBoardList.forEach(System.out::println);
+        } catch (IOException | SAXException | InvalidFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
+
